6fb870a1001ecdcf77979b6e7702fca004dfe7df
Thomas Hils <thomashils@Thomass-MacBook-Air.local>
2014-08-13 20:30:58 -0400

    added heroku

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,18 +8,49 @@
  * Controller of the taskBlocsApp
  */
 
+
+// {{todo.age}}
+//
+
+
+
+
 angular.module('taskBlocsApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = [];
-
+    setInterval(function () {
+      //loop over $scope.todos and set age (every minute right now)
+    }, 60000);
     $scope.addTodo = function() {
-      var expirationDate = 'Test';
-      $scope.todos.push($scope.todo);
-      $scope.todo.expirationdate = expirationDate;
-      console.log($scope.todo.expirationdate);
-      $scope.todo = '';
+      var creationDate = moment();
+//      var expirationDate = moment().add(7, 'days');
+      var today = moment('08-25-2014', 'MM-DD-YYYY');
+      var age = today.diff(creationDate, 'days');
+      
+      //$scope.todo.name = $scope.title;
+      //$scope.todo.age = age;
+
+      $scope.todos.sort(function (a, b) {
+        return a.i > b.i;
+      });
+      
+      $scope.todos.push(
+      {
+        name: $scope.title,
+        creationDate: creationDate,
+        age: age
+      }
+      );
+      $scope.title = '';
+ //   
+      $scope.todos.each(function(item) {
+        item.age = today.diff(item.creationDate, 'days');
+      });
+     
     };
     $scope.removeTodo = function (index) {
       $scope.todos.splice(index, 1);
     };
   });
+
+
